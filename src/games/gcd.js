99f3b2e9ff2bc625958b124131f34d1b717230b3
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-04-30 00:16:19 +0400

    update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,19 +1,22 @@
-import { game } from "../gameTemplate.js";
+import { game } from '../gameTemplate.js';
 
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // максимум и минимум включаются
-};
+  let minValue = min;
+  let maxValue = max;
+  minValue = Math.ceil(min);
+  maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+  // максимум и минимум включаются
+}
 
-const description = "Find the greatest common divisor of given numbers.";
+const description = 'Find the greatest common divisor of given numbers.';
 
 const getGcd = (number1, number2) => {
   while (number1 !== number2) {
     if (number1 > number2) {
-      number1 = number1 - number2;
+      number1 -= number2;
     } else {
-      number2 = number2 - number1;
+      number2 -= number1;
     }
   }
   return (number1); 
@@ -24,7 +27,7 @@ const getAnswerAndQuestion = () => {
   const number2 = getRandomIntInclusive(0, 100);
   const question = `${number1} ${number2}`;
   const answer = getGcd(number1, number2);
-
+  
   return [answer, question];
 };
 
