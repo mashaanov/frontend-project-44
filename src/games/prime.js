d8fcb6a2a064c6d5df72c520a1f398a56d789ce6
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-04-29 17:50:40 +0400

    update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,13 +1,15 @@
-import { game } from "../gameTemplate.js";
+import { game } from ''../gameTemplate.js';
 
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // максимум и минимум включаются
-};
+  let minValue = min;
+  let maxValue = max;
+  minValue = Math.ceil(min);
+  maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue); 
+  // максимум и минимум включаются
+}
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-
 
 const isPrime = (number) => {
   let result = 'yes';
@@ -18,17 +20,16 @@ const isPrime = (number) => {
     for (let divider = 2; divider < Math.sqrt(number) + 1; divider += 1) {
       if (number % divider === 0) {
         result = 'no';
-        break;
-      } 
+      }
     };
-  }; 
+  }
   return result;
 };
 
 const getAnswerAndQuestion = () => {
-  const question = getRandomIntInclusive(0,100);
+  const question = getRandomIntInclusive(0, 100);
   const answer = isPrime(question);
   return [answer, question];
-}
+};
 
 export default () => game(description, getAnswerAndQuestion);
