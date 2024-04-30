314be335856481fed0ffbdd654c9a97f28fc4e1f
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-04-30 13:14:49 +0400

    update gameTemplate.js

--- src/gameTemplate.js
+++ src/gameTemplate.js
@@ -4,31 +4,27 @@ const countRound = 3;
 let i = 0;
 
 export const game = (description, getAnswerAndQuestion) => {
-  console.log("Welcome to the Brain Games!");
-  const userName = readlineSync.question("May I have your name? ");
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(`${description}`);
 
   for (i; i < countRound; i += 1) {
     const [answer, question] = getAnswerAndQuestion();
     console.log(`Question: ${question}`);
-    let userAnswer = readlineSync.question("Your answer: ")
-    const formattedUserAnswer = (typeof(answer) === 'number' ? +userAnswer: userAnswer);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const formattedUserAnswer = (typeof answer === 'number' ? +userAnswer : userAnswer);
 
-    if (formattedUserAnswer  === answer) {
-      console.log("Correct!");
+    if (formattedUserAnswer === answer) {
+      console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
       console.log(`Let's try again, ${userName}!`);
-      break
+      break;
     }
-  };
+  }
 
   if (i === countRound) {
     console.log(`Congratulations, ${userName}!`);
   }
 };
-
-
-
-
