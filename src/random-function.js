a48bce2ac999856742aa50ff79aa27794b124279
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-05-08 21:38:34 +0400

    update random-function.js

--- src/random-function.js
+++ src/random-function.js
@@ -1,9 +1,9 @@
 function getRandomIntInclusive(min, max) {
-    let minValue = min;
-    let maxValue = max;
-    minValue = Math.ceil(min);
-    maxValue = Math.floor(max);
-    return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
-    // максимум и минимум включаются
-};
+  let minValue = min;
+  let maxValue = max;
+  minValue = Math.ceil(min);
+  maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+  // максимум и минимум включаются
+}
 export default getRandomIntInclusive;
